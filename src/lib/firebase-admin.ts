d1e72231eb50dbaeba53fca121f4334ef55ef0b8
Phoinix-PaymentSolutions <contact@phoinix.nl>
2025-10-05 15:08:58 +0200

    Rewrote firebase-admin.ts

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,12 +1,16 @@
 // src/lib/firebase-admin.ts
 import * as admin from 'firebase-admin';
-import serviceAccount from '../../serviceAccountKey.json';
 
 const apps = admin.apps;
 
 if (!apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      // The replace fixes newline characters in the key
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    })
   });
 }
 
